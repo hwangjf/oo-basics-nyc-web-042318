ced1d6617be79fbcd16ec176bdd24dd662b4c788
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-25 21:14:48 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,4 @@
 class Book
-  def initialize(title, author, page_count, genre)
-    @title = title
-    @author = author
-    @page_count = page_count
-    @genre = genre
-  end
   
-  def turn_page
-    @page_count += 1
-  end
 end
 
